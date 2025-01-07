be71d6d4e8c894818b6ce7586901aeb9578ac6cc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-07 18:55:54 +0300

    Create part-c-0005-type-casting1.kt

---
+++ part-c-0005-type-casting1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    
+    val a = 101
+    
+    val b = 101.11 
+    
+    val c = "21"
+    
+    println("${a.toDouble()}")
+    
+    println("${a.toString()}")
+    
+    println("${b.toInt()}")
+    
+    println("${b.toString()}")
+    
+    val d = c.toInt()
+    
+    println("a + b + d = ${a + b + d}") // a + b + d = 223.11
+
+}
+
