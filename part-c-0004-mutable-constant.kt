3fb87d91bbfd23caef56b8b28daa7d1870912440
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-07 18:48:28 +0300

    Create part-c-0004-mutable-constant.kt

---
+++ part-c-0004-mutable-constant.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    var a = 11 // mutable value
+    
+    println("a = $a")
+   
+   	a = 101
+    
+	  println("a = $a")
+    
+    val b = 212 // constant-immutable value
+    
+    println("b = $b")
+    
+    //b = 2234 // Error => Val cannot be reassigned
+
+}
+
+// a = 11
+// a = 101
+// b = 212
