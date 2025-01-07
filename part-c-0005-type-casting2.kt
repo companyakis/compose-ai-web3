ae47c4deb78e5779092f93cff58d933e5db99da4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-07 19:00:32 +0300

    Create part-c-0005-type-casting2.kt

---
+++ part-c-0005-type-casting2.kt
@@ -0,0 +1,24 @@
+fun main() {
+    
+    val a = "21"
+    
+    val hi = "Hi!"
+    
+    val x = a.toIntOrNull()
+    
+    val y = a.toDoubleOrNull()
+    
+    val z = hi.toIntOrNull()
+    
+	println(x)
+    
+    println(y)
+    
+    println(z)
+
+}
+
+// 21
+// 21.0
+// null
+
