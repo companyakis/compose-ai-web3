169f2a084b97511d2c9414b67e2d200ede62bc43
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-07 18:45:05 +0300

    Create part-c-0003-basic-calculus.kt

---
+++ part-c-0003-basic-calculus.kt
@@ -0,0 +1,22 @@
+fun main() {
+    
+    var a = 11 
+    
+    var b = 3 
+    
+    println("$a + $b = ${a + b}")
+   	println("$a - $b = ${a - b}")
+    println("$a * $b = ${a * b}")
+    println("$a / $b = ${a / b}")
+    
+    var modAandB: Int = a % b
+    
+    println("$a % $b = ${modAandB}")
+
+}
+
+// 11 + 3 = 14
+// 11 - 3 = 8
+// 11 * 3 = 33
+// 11 / 3 = 3
+// 11 % 3 = 2
