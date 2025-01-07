1b6292f23bde7599c19753ead3faa66aeb828e8c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-07 18:32:53 +0300

    Create part-c-0001-basic-types.kt

---
+++ part-c-0001-basic-types.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    var firstName = "Mustafa"
+    
+    var lastName: String = "Büyükdereli"
+    
+    var year = 2025
+    
+    var kotlinLessonCode: Int = 101
+    
+    var piNumber: Double = 3.14 
+    
+    var piNumberFloat: Float = 3.14f
+    
+    var firstLetter: Char = 'A'
+    
+    var isReady = true
+    
+    var isCompleted: Boolean = false
+    
+}
