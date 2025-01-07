f14506dde61ee841c82ac455263f14885c6a998a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-07 18:37:53 +0300

    Create part-c-0002-print-comments.kt

---
+++ part-c-0002-print-comments.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+    // print
+    
+    var firstName = "Mustafa"
+    
+    var lastName: String = "Büyükdereli"
+    
+    var year = 2025
+    
+    println(firstName)
+    
+    println("$firstName $lastName $year...")  
+    
+}
+
+/*
+Mustafa
+Mustafa Büyükdereli 2025...
+*/
