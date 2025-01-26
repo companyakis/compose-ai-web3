9f3b23fc3ee90b15f654d355bf83237e1a24034d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-26 20:27:56 +0300

    Create part-c-0008-conditionals1.kt

---
+++ part-c-0008-conditionals1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    
+    var x = 100
+    
+    var y = 50
+    
+    if (x > y) { println("First number is bigger") }
+    
+    else if (y > x) { println("Second number is bigger") }
+    
+    else { println("Two numbers are equal")}
+    
+}
+
+
+
