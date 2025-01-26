f1be1b8de432a46da64321e4ded429c6e5575aba
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-26 18:48:32 +0300

    Create part-c-0006-strings1.kt

---
+++ part-c-0006-strings1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    
+    val whoAmI: String = "Mustafa Büyükdereli"
+    
+    val myName = whoAmI.substring(0, 7)
+    
+    println("My name is $myName!") // My name is Mustafa!
+
+}
