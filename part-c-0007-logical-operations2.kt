bf2f6cd2cffc6912476939a54bbc4532d9793f92
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-26 20:10:30 +0300

    Create part-c-0007-logical-operations2.kt

---
+++ part-c-0007-logical-operations2.kt
@@ -0,0 +1,27 @@
+fun main() {
+    
+    var x = 100
+    
+    var y = 50
+    
+    println("Condition 1: ${x > y}")
+    
+    println("Condition 2: ${x >= y}")
+    
+	println("Condition 3: ${x < y}")
+    
+    println("Condition 4: ${x <= y}")
+    
+    println("Condition 5: ${x == y}")
+    
+    println("Condition 6: ${x != y}")
+
+}
+
+// Condition 1: true
+// Condition 2: true
+// Condition 3: false
+// Condition 4: false
+// Condition 5: false
+// Condition 6: true
+
