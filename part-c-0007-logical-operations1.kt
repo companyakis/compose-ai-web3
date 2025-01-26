04553f5e8d187aef0267a09c846f83e763a0d51c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-26 20:06:06 +0300

    Create part-c-0007-logical-operations1.kt

---
+++ part-c-0007-logical-operations1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    
+    var x = true
+    
+    var y = false
+    
+    println("Condition 1: ${x && y}")
+    
+    println("Condition 2: ${x || y}")
+    
+	  println("Condition 3: ${!x}")
+    
+    println("Condition 3: ${!y}")
+
+}
+
+// Condition 1: false
+// Condition 2: true
+// Condition 3: false
