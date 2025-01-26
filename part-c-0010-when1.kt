4d145041c1f235b50bb3d85f0318e996d340954b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-26 20:47:35 +0300

    Create part-c-0010-when1.kt

---
+++ part-c-0010-when1.kt
@@ -0,0 +1,18 @@
+fun main() {
+    
+    val summerMonth = 3 
+    
+    when (summerMonth) {
+        
+        6 -> println("June")
+        
+        7 -> println("July")
+        
+        8 -> println("August")
+        
+        else -> println("Not a summer month!")
+    }
+    
+}
+
+
