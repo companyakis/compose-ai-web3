4dd511fbef05d8034b5e85ff3d9dc36e6b9367eb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-26 21:42:30 +0300

    Create part-c-0011-while1.kt

---
+++ part-c-0011-while1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    
+    var counter = 7
+    
+    while (counter < 25) {
+        
+        println("Counter value: $counter")
+        
+        counter += 2
+    }
+    
+}
+
+// Counter value: 7
+// Counter value: 9
+// Counter value: 11
+// Counter value: 13
+// Counter value: 15
+// Counter value: 17
+// Counter value: 19
+// Counter value: 21
+// Counter value: 23
