751abc72b76b1713c794bc32777cded7935ee803
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-17 11:04:19 +0300

    Create part-c-0017-byte-short-long.kt

---
+++ part-c-0017-byte-short-long.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    
+    val herAge: Byte = 35
+    
+    println(herAge)
+    
+    val year: Short = 2025
+    
+    println(year)
+    
+    val bigNum: Long = -922337203685775808
+    
+    print(bigNum)    
+
+}
