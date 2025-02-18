aab4026c74dfb587ea88a1fe33fe6e95a53b46ff
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-18 12:57:10 +0300

    Create part-c-0019-unsigned-integers.kt

---
+++ part-c-0019-unsigned-integers.kt
@@ -0,0 +1,23 @@
+fun main() {
+    
+    // unsigned integers -> not negative numbers!
+    
+    val age: UByte = 35u 
+    
+    val year: UShort = 2025u 
+    
+    val budget: UInt = 580000u 
+    
+    var totalSales: ULong = 52500000000u
+    
+    println(age)
+    println(year)
+    println(budget)
+    println(totalSales)
+    
+// 35
+// 2025
+// 580000
+// 52500000000
+    
+}
