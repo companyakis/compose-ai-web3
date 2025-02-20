e5dcba57ee4e997b034eb396f32c83bd81344a24
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-20 23:22:55 +0300

    Create part-c-0015-arraylist4.kt

---
+++ part-c-0015-arraylist4.kt
@@ -0,0 +1,12 @@
+fun main() {
+    
+    val empAyhan = arrayListOf<Any>()
+    
+    empAyhan.add("Ayhan Bilir")
+    empAyhan.add("Marketing")
+    empAyhan.add(101)
+    empAyhan.add(true)
+    empAyhan.add(55000)
+
+}
+
