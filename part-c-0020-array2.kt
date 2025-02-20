086ee257b450a4b36ed3c26fb33707591cf52b05
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-20 22:57:03 +0300

    Create part-c-0020-array2.kt

---
+++ part-c-0020-array2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    
+    val empData = arrayOf("Ayhan Bilir", "Sales", 65000, 101, false)
+    
+    println("Name: ${empData[0]}")
+    println("Department: ${empData[1]}")
+    println("Salary: ${empData[2]} ₺")
+    println("ID: ${empData[3]}")
+    println("Marriage: ${empData[4]}")
+    
+}
+
+// Name: Ayhan Bilir
+// Department: Sales
+// Salary: 65000 ₺
+// ID: 101
+// Marriage: false
