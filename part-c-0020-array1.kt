174dd568c8ea03b4b62a885d04f35e52fe03fe61
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-20 22:52:49 +0300

    Create part-c-0020-array1.kt

---
+++ part-c-0020-array1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    
+    val names = arrayOf("ayhan", "bill", "kağan", "george")
+
+    println(names[2]) // kağan
+    
+    names[3] = "kutluk"
+    
+    println(names[3]) // kutluk
+    
+    val years = arrayOf(2021, 2034, 2016, 2018)
+
+}
