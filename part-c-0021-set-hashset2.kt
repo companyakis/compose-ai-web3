4a52f18a5ac348ccbe469c40bb5219e6738895ca
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-24 12:31:57 +0300

    Create part-c-0021-set-hashset2.kt

---
+++ part-c-0021-set-hashset2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    
+    val numbers = setOf(100, 150, 150, 200, 100, 100, 200)
+    
+    println(numbers.size) // 3
+    
+    numbers.forEach() {
+        println(it)
+    }
+    
+// 100
+// 150
+// 200
+
+}
