510e48ee5ea46beb231d3313a59ab228d83097fc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 10:39:38 +0300

    Create part-c-0024-break-continue2.kt

---
+++ part-c-0024-break-continue2.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    for (i in 2008..2025){
+        
+        if (i == 2018) {
+            continue
+        }
+        println("Year: $i")
+    }
+
+    println("Ended!")
+}
+
+/*
+
+Year: 2008
+Year: 2009
+Year: 2010
+Year: 2011
+Year: 2012
+Year: 2013
+Year: 2014
+Year: 2015
+Year: 2016
+Year: 2017
+Year: 2019
+Year: 2020
+Year: 2021
+Year: 2022
+Year: 2023
+Year: 2024
+Year: 2025
+Ended!
+
+*/
