9b8771ef6720af0d8f2d04a2a5304fb8a7847e0d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 11:41:23 +0300

    Create part-c-0013-class-oop0.kt

---
+++ part-c-0013-class-oop0.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val emp_ayhan = Employee("Ayhan Bilir", "HR", "Expert", 65000)
+
+    println(emp_ayhan.name)
+
+    emp_ayhan.department = "Sales"
+
+    println(emp_ayhan.department)
+
+}
+
+class Employee(var name: String, var department: String, var title: String, salary: Int) {
+
+}
+
+
+
