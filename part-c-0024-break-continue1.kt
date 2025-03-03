e3a81c5dcf6134608e9d413e2a8b90a6395b2b76
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 10:38:08 +0300

    Create part-c-0024-break-continue1.kt

---
+++ part-c-0024-break-continue1.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    for (i in 2008..2025) {
+        println("Year: $i")
+
+        if (i == 2018) {
+            break
+        }
+    }
+
+    println("Ended!")
+}
+
+/*
+Year: 2008
+Year: 2009
+Year: 2010
+Year: 2011
+Year: 2012
+Year: 2013
+Year: 2014
+Year: 2015
+Year: 2016
+Year: 2017
+Year: 2018
+Ended!
+*/
