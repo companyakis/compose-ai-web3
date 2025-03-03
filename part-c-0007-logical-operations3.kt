e7dcf808e711db2a30609babb8fe121b49a49260
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 10:15:57 +0300

    Create part-c-0007-logical-operations3.kt

---
+++ part-c-0007-logical-operations3.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val a = 25
+    val b = 12
+
+    val x = 37
+    val y = 25
+
+    // or - and -> multiple comparisons
+
+    println(a > b && x != y) // true
+
+    println(a == b || x > y) // true
+
+    println(a >= b && x == y) // false
+
+}
