609c4a273419b39be8117a914a729a4b8b6e02e1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 10:24:59 +0300

    Create part-c-0010-when4.kt

---
+++ part-c-0010-when4.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val name = "MUSTAFA"
+
+    when (name.lowercase()) {
+
+        "mustafa" -> println("FinTech")
+        "aygün" -> println("Operations")
+        "bengü" -> println("Sales")
+        "ayhan" -> println("Marketing")
+        "kağan" -> println("Accounting")
+
+        else -> println("Unknown person!")
+    }
+
+    
+}
