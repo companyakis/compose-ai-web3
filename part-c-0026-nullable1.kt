53890f207777f339b65116170fe7ff19d9ce0b48
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-04 11:30:38 +0300

    Create part-c-0026-nullable1.kt

---
+++ part-c-0026-nullable1.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val userName: String? = null
+
+    println("User name: $userName")
+
+    // if we are sure!
+
+    var city: String? = null
+
+    city = "İzmir"
+
+    println("City: $city!!")
+
+}
+
+// User name: null
+// City: İzmir!!
