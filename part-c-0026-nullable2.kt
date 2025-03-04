2992a11ea3dcc22b0f5aa65a4b18826f215e8589
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-04 11:33:29 +0300

    Create part-c-0026-nullable2.kt

---
+++ part-c-0026-nullable2.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val userName: String? = null
+
+    if (userName != null) {
+
+        println("USer name: $userName")
+    }
+
+    else {
+
+        println("User name not found!")
+    }
+
+}
+
