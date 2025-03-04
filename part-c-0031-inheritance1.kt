83d7f1f9fbf585d63a70256e7b114787511f639f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-04 22:35:08 +0300

    Update part-c-0031-inheritance1.kt

--- part-c-0031-inheritance1.kt
+++ part-c-0031-inheritance1.kt
@@ -4,6 +4,8 @@ fun main() {
 
     val employeeAyhan = Employee("Ayhan Bilir", "4298", 57000, salesDepartment.departmentName)
 
+    val empHakan = Employee("Hakan Göktürk", "6587", 65000, "HR")
+
     println("Employee: ${employeeAyhan.empName}")
     println("Employee ID: ${employeeAyhan.ID}")
     println("Employee salary: ${employeeAyhan.salary} ₺")
@@ -11,15 +13,16 @@ fun main() {
 
 }
 
-// super
-open class Department(var departmentName: String) { 
+
+open class Department(var departmentName: String) {
 
 }
 
-//sub
 class Employee(var empName: String, var ID: String, var salary: Int,  departmentName: String) : Department(departmentName) {
 
 }
+
+
 
 // Employee: Ayhan Bilir
 // Employee ID: 4298
