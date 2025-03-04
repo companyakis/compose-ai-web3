e5c5d6f25f46d1fdd862a478d8b434cc755de4fc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-04 10:58:27 +0300

    Create part-c-0025-extension2.kt

---
+++ part-c-0025-extension2.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    val result1 = 3 multAndAddOne 7
+
+    println("Result 1: $result1")
+
+    val result2 = 11 multAndAddOne 15
+
+    println("Result 2: $result2")
+
+}
+
+// f(x) = n * x + 1
+
+// this -> Int class
+
+infix fun Int.multAndAddOne(x: Int) : Int {
+
+    return this * x + 1
+}
+
+// Result 1: 22
+// Result 2: 166
