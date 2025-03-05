b233c9b53a60130a34c0bdbd55683565df33336c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-05 12:46:26 +0300

    Create part-c-0036-filter2.kt

---
+++ part-c-0036-filter2.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    val emp1 = Employee("Aykan Alıcı", 4296, "Sales", 57000)
+    val emp2 = Employee("Kağan Gökada", 3254, "Marketing", 62500)
+    val emp3 = Employee("Bengü Bilir", 6578, "Accounting", 48000)
+
+    val empList = ArrayList<Employee>()
+
+    empList.addAll(arrayOf(emp1, emp2, emp3))
+
+    for (emp in empList.filter { it.department.contains("t")}) {
+
+        println("${emp.name} - ${emp.id} - ${emp.department} - ${emp.salary}")
+    }
+
+}
+
+data class Employee(var name: String, var id: Int, var department: String, var salary: Int) {
+
+}
+
+// Kağan Gökada - 3254 - Marketing - 62500
+// Bengü Bilir - 6578 - Accounting - 48000
