aa60483dcfc536db16ad8dd1fb6bf81d4583d765
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-05 12:38:35 +0300

    Create part-c-0035-sorted-with1.kt

---
+++ part-c-0035-sorted-with1.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    val emp1 = Employee("Aykan Alıcı", 4296, "Sales", 57000)
+    val emp2 = Employee("Kağan Gökada", 3254, "Marketing", 62500)
+    val emp3 = Employee("Bengü Bilir", 6578, "Accounting", 48000)
+
+    val empList = ArrayList<Employee>()
+
+    empList.addAll(arrayOf(emp1, emp2, emp3))
+
+    for (emp in empList.sortedWith(compareBy { it.salary })) {
+
+        println("${emp.name} - ${emp.id} - ${emp.department} - ${emp.salary}")
+    }
+
+}
+
+data class Employee(var name: String, var id: Int, var department: String, var salary: Int) {
+
+}
+
+// Bengü Bilir - 6578 - Accounting - 48000
+// Aykan Alıcı - 4296 - Sales - 57000
+// Kağan Gökada - 3254 - Marketing - 62500
