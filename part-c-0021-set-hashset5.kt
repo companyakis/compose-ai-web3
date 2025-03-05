48ff0521c36f14d52418f633d07091a19e67d3d9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-05 13:02:35 +0300

    Create part-c-0021-set-hashset5.kt

---
+++ part-c-0021-set-hashset5.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val names = HashSet<String>()
+
+    names.addAll(listOf("Mustafa", "Mustafa", "Aygün", "Bilge", "Kültigin", "Bengü", "Merve"))
+
+    names.remove("Bilge")
+
+    names.clear()
+
+}
+
+
+
+
