6b2d660f6e422bcfe180b29a12cf2a8698797549
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-11 14:37:54 +0300

    Create part-b-0005-arrangement-alignment1.kt

---
+++ part-b-0005-arrangement-alignment1.kt
@@ -0,0 +1,45 @@
+package org.example.design
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Box(modifier = Modifier.size(110.dp).background(Color.Blue))
+            Box(modifier = Modifier.size(85.dp).background(Color.Green))
+            Box(modifier = Modifier.size(140.dp).background(Color.Magenta))
+        }
+
+
+    }
+}
