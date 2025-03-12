a178450d4b3ded11566610d2c03bd293589b9d86
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-12 09:38:10 +0300

    Create part-b-0006-color1.kt

---
+++ part-b-0006-color1.kt
@@ -0,0 +1,16 @@
+package org.example.design
+
+import androidx.compose.ui.graphics.Color
+
+// https://color.adobe.com/trends
+
+
+val colorPink = Color(0xFFD959C3)
+
+val colorBackOne = Color(0xFF161426)
+
+val colorBackTwo = Color(0xFF0D0D0D)
+
+val colorBlue1 = Color(0xFF186BD9)
+
+val colorBlue2 = Color(0xFF165FF2)
