872c79162f6b283a14c774cc0f59ff9861e7b29f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-12 10:43:08 +0300

    Create part-b-0007-font4-App.kt

---
+++ part-b-0007-font4-App.kt
@@ -0,0 +1,51 @@
+// let's use our font => roboto
+
+package org.example.design
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+
+        ) {
+
+            // let's use our font family => roboto
+
+            Text(text = "AI and Web3", color = colorBlue2, fontSize = 35.sp, fontFamily = GetRobotoFontFamily())
+
+
+        }
+
+
+    }
+}
