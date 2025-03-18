08cf56dc55ffae0a8e2c876680a961734eaa5374
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-18 13:30:12 +0300

    Create part-b-0020-spacer2.kt

---
+++ part-b-0020-spacer2.kt
@@ -0,0 +1,50 @@
+package org.example.design
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+        Row(
+            modifier = Modifier.fillMaxSize(),
+
+
+        ) {
+
+            Spacer(Modifier.size(15.dp))
+
+            Box(Modifier.size(80.dp, 110.dp).background(Color.Blue))
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Box(modifier = Modifier.size(130.dp, 60.dp).background(Color.Green))
+
+            Spacer(Modifier.size(35.dp, 20.dp))
+
+            Box(Modifier.size(75.dp, 91.dp).background(Color.DarkGray))
+
+        }
+
+    }
+}
