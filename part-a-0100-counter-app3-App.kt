f0ef6bf12921c674ae55c02e8446e352fda697cd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-19 12:52:49 +0300

    Create part-a-0100-counter-app3-App.kt

---
+++ part-a-0100-counter-app3-App.kt
@@ -0,0 +1,19 @@
+package org.example.workstructure
+
+
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.runtime.*
+
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+        MainPage()
+
+    }
+}
